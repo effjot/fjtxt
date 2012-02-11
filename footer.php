820524a405d60fdf9e1e9ddfccb8526c7ce4e32c
Florian Jenn <devel@effjot.net>
2012-02-11 21:09:05 +0100

    Re-indented, clean-up.  Added copyright and translation notice.

--- footer.php
+++ footer.php
@@ -1,11 +1,51 @@
+<div id="footer">
 
-	<div id="footer">
-		<span id="theme-link"><a href="http://www.plaintxt.org/themes/blogtxt/" title="<?php _e('blog.txt theme for WordPress', 'blogtxt' ) ?>" rel="follow designer">blog.txt</a> <?php _e('theme by', 'blogtxt') ?> <span class="vcard"><a class="url fn n" href="http://scottwallick.com/" title="scottwallick.com" rel="follow designer"><span class="given-name">Scott</span><span class="additional-name"> Allan</span><span class="family-name"> Wallick</span></a></span></span>
-	</div><!-- #footer -->
+  <p class="alignleft" style="margin-top: 0.75ex; padding-right: 1em;">
+    <a class="nodecoration" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/">
+      <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/de/88x31.png" />
+    </a>
+  </p>
+
+  <p style="margin-top: 0;">
+    <span xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/Text"
+          property="dc:title" rel="dc:type">blog.effjot.net</span>
+    steht unter einer <a rel="license"
+    href="http://creativecommons.org/licenses/by-nc-sa/3.0/de/">Creative
+    Commons Namensnennung-Keine kommerzielle Nutzung-Weitergabe unter
+    gleichen Bedingungen 3.0 Deutschland Lizenz</a>.
+  </p>
+
+  <p>
+    <span id="theme-link">
+      <?php _e('Theme based on', 'blogtxt') ?>
+      <a href="http://www.plaintxt.org/themes/blogtxt/" title="<?php _e('blog.txt theme for WordPress', 'blogtxt' ) ?>" rel="follow designer">
+        blog.txt
+      </a>
+      <?php _e('by', 'blogtxt') ?>
+      <span class="vcard">
+        <a class="url fn n" href="http://scottwallick.com/"
+           title="scottwallick.com" rel="follow designer">
+          <span class="given-name">Scott</span>
+          <span class="additional-name">Allan</span>
+          <span class="family-name">Wallick</span>
+        </a>
+      </span>,
+
+      <?php _e('modified and translated by', 'blogtxt') ?>
+      <span class="vcard">
+        <a class="url fn n" href="http://effjot.net/" title="effjot.net" rel="follow designer">
+          <span class="given-name">Florian</span>
+          <span class="family-name">Jenn</span>
+        </a>
+      </span>.
+    </span>
+  </p>
+
+</div><!-- #footer -->
 
 </div><!-- #wrapper -->
 
-<?php wp_footer() // Do not remove; helps plugins work ?>
+<?php wp_footer(); ?>
 
-</body><!-- end transmission -->
+</body>
 </html>
