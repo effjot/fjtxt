48a59a72d85c22500d9e91ab4aec630c437068ce
Florian Jenn <devel@effjot.net>
2012-02-18 17:39:44 +0100

    Re-indented and clean-up.

--- page.php
+++ page.php
@@ -1,29 +1,43 @@
 <?php get_header() ?>
 
-			<div class="hfeed">
+<div class="hfeed">
 
-<?php the_post() ?>
+  <?php the_post() ?>
 
-				<div id="post-<?php the_ID(); ?>" class="<?php blogtxt_post_class() ?>">
-					<h2 class="entry-title"><?php the_title(); ?></h2>
-					<?php if ( get_post_custom_values('authorlink') ) printf(__('<div class="archive-meta">By %1$s</div>', 'blogtxt'), blogtxt_author_link() ) // Add a key/value of "authorlink" to show an author byline on a page ?>
-					<div class="entry-content">
-<?php the_content() ?>
+  <div id="post-<?php the_ID() ?>" class="<?php blogtxt_post_class() ?>">
 
-<?php wp_link_pages('before=<div class="page-link">' . __('Pages: ', 'blogtxt') .
+    <h2 class="entry-title"><?php the_title() ?></h2>
+
+    <?php
+      if (get_post_custom_values('authorlink'))
+        printf(__('<div class="archive-meta">By %1$s</div>', 'blogtxt'), blogtxt_author_link())
+          // Add a key/value of "authorlink" to show an author byline on a page
+    ?>
+
+    <div class="entry-content">
+
+      <?php the_content() ?>
+
+      <?php 
+        wp_link_pages('before=<div class="page-link">' . __('Pages: ', 'blogtxt') .
                       '&after=</div>&next_or_number=number');
- ?>
 
-<?php edit_post_link(__('Edit this entry.', 'blogtxt'),'<p class="entry-edit">','</p>') ?>
+        edit_post_link(__('Edit this entry.', 'blogtxt'), '<p class="entry-edit">', '</p>')
+      ?>
 
-					</div>
-				</div><!-- .post -->
+    </div>
 
-<?php if ( get_post_custom_values('comments') ) comments_template() // Add a key/value of "comments" to load comments on a page ?>
+  </div>
 
-			</div><!-- .hfeed -->
-		</div><!-- #content -->
-	</div><!-- #container -->
+  <?php 
+    if (get_post_custom_values('comments')) comments_template()
+      // Add a key/value of "comments" to load comments on a page
+  ?>
+
+</div>
+
+</div><!-- #content -->
+</div><!-- #container -->
 
 <?php get_sidebar() ?>
 <?php get_footer() ?>
